ff35e6e5232b2dc67b7ebea9e1b9c89d7721749e
Faridatou Chaibou G <chaibou64yahoo.fr@MacBook-Air-de-farida.local>
2024-11-11 18:37:47 +0100

    modification de la barre de navigation de la page de gestion des etudiants

--- view/students/index.php
+++ view/students/index.php
@@ -18,16 +18,17 @@
 <body>
 
   <!-- Barre de navigation -->
-  <header class="bg-light py-3">
+  <header class="bg-dark text-white py-3">
     <div class="container d-flex justify-content-between align-items-center">
-      <h1 class="h3 text-primary">Gestion d'École</h1>
+      <h1 class="h3">Gestion d'École</h1>
       <nav>
-        <a href="#" class="text-dark me-3">Accueil</a>
-        <a href="#" class="text-dark me-3">Étudiants</a>
-        <a href="#" class="text-dark">Contact</a>
+        <a href="#" class="text-white me-3">Accueil</a>
+        <a href="#" class="text-white me-3">Fonctionnalités</a>
+        <a href="#" class="text-white">Contact</a>
       </nav>
     </div>
   </header>
+
 
   <!-- Container principal -->
   <div class="container table-container">
